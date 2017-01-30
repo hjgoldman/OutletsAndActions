80b8c002766a7ad261ff17b271b4d0a9fe47f3f0
taargus <haydengoldman@gmail.com>
2017-01-30 13:48:35 -0600

    Initial Commit

---
+++ OutletsAndActions/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OutletsAndActions
+//
+//  Created by Hayden Goldman on 1/30/17.
+//  Copyright © 2017 Hayden Goldman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
